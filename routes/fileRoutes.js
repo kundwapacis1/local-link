58bb9fa9cf7ff06f85afdc60d5186fb65b8c5303
kundwa pacis1 <gihozokundwa250@gmail.com>
2025-11-07 17:46:30 +0200

    for the main branch

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { uploadFile } from "../controllers/fileControllers";
+import { uploadFile, getFiles, downloadFile} from "../controllers/fileControllers";
 import upload from "../middleware/uploadMiddleware";
 
 const router  = express.Router();
 
 router.post("upload", upload.single("file"), uploadFile);
+router.get("/", getFiles);
+router.get("/download/:id", downloadFile);
  export default router;
